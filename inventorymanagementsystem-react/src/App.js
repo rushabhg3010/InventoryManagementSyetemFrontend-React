75d348b416a547aa4a227f0c5ddf1d0f08e35f08
rushabhg3010 <rushabhg3010@gmail.com>
2026-05-26 23:45:24 +0530

    Created a register user path

--- inventorymanagementsystem-react/src/App.js
+++ inventorymanagementsystem-react/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { ProtectedRoute, AdminRoute } from "./service/GuardService";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<RegisterPage/>}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter> 
+  )
 }
 
 export default App;
