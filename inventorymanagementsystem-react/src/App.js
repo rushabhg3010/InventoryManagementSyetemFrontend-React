ed100a3f2a204eeffb570885bea9d76b12a5c02b
rushabhg3010 <rushabhg3010@gmail.com>
2026-05-28 17:22:55 +0530

    Added AddEditSupplierPage in route in APP.js file

--- inventorymanagementsystem-react/src/App.js
+++ inventorymanagementsystem-react/src/App.js
@@ -5,6 +5,7 @@ import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import CategoryPage from "./pages/CategoryPage";
 import SupplierPage from "./pages/Supplier";
+import AddEditSupplierPage from "./pages/AddEditSupplierPage";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
         {/* ADMIN ROUTES */}
         <Route path="/category" element={<AdminRoute element={<CategoryPage />} />}></Route>
         <Route path="/supplier" element={<AdminRoute element={<SupplierPage />} />}></Route>
+        <Route path="/add-supplier" element={<AdminRoute element={<AddEditSupplierPage />} />}></Route>
+        <Route path="/edit-supplier/:supplierId" element={<AdminRoute element={<AddEditSupplierPage/>}/>}></Route>
       </Routes>
     </BrowserRouter>
   )
