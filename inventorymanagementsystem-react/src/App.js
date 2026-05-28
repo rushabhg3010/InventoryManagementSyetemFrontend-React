f1d9f084e86ef4b9bbeeacabee035bdb2f6ed680
rushabhg3010 <rushabhg3010@gmail.com>
2026-05-28 16:28:54 +0530

    Added Category Page in Route

--- inventorymanagementsystem-react/src/App.js
+++ inventorymanagementsystem-react/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-rou
 import { ProtectedRoute, AdminRoute } from "./service/GuardService";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import CategoryPage from "./pages/CategoryPage";
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/register" element={<RegisterPage/>}></Route>
         <Route path="/login" element={<LoginPage/>}></Route>
+        
+         {/* ADMIN ROUTES */}
+        <Route path="/category" element={<AdminRoute element={<CategoryPage/>}/>}></Route>
       </Routes>
     </BrowserRouter> 
   )
