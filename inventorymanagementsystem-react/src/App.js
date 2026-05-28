5b420e4fe84e00c7f2f513cd9588e8b5aa424402
rushabhg3010 <rushabhg3010@gmail.com>
2026-05-28 16:56:15 +0530

    Routed Supplier page in App.js file

--- inventorymanagementsystem-react/src/App.js
+++ inventorymanagementsystem-react/src/App.js
@@ -4,18 +4,20 @@ import { ProtectedRoute, AdminRoute } from "./service/GuardService";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import CategoryPage from "./pages/CategoryPage";
+import SupplierPage from "./pages/Supplier";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<RegisterPage/>}></Route>
-        <Route path="/login" element={<LoginPage/>}></Route>
-        
-         {/* ADMIN ROUTES */}
-        <Route path="/category" element={<AdminRoute element={<CategoryPage/>}/>}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+
+        {/* ADMIN ROUTES */}
+        <Route path="/category" element={<AdminRoute element={<CategoryPage />} />}></Route>
+        <Route path="/supplier" element={<AdminRoute element={<SupplierPage />} />}></Route>
       </Routes>
-    </BrowserRouter> 
+    </BrowserRouter>
   )
 }
 
