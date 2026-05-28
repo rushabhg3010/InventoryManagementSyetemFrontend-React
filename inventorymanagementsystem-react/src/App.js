9f1e3431f7c58dc004626473ee69fb9054a08754
rushabhg3010 <rushabhg3010@gmail.com>
2026-05-28 15:35:05 +0530

    Added Register Page and Login Page in Routes

--- inventorymanagementsystem-react/src/App.js
+++ inventorymanagementsystem-react/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import { ProtectedRoute, AdminRoute } from "./service/GuardService";
 import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<RegisterPage/>}>
-
-        </Route>
+        <Route path="/register" element={<RegisterPage/>}></Route>
+        <Route path="/login" element={<LoginPage/>}></Route>
       </Routes>
     </BrowserRouter> 
   )
